d3b5f190bb9ebb74f7240a02ef59718140030a6e
Nyan-Min-Htet <nhtet9968@gmail.com>
2025-03-21 22:04:12 +0630

    first commit

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,33 +6,37 @@ const Home = () => {
     <>
       <section
         id="home"
-        className="py-45 min-h-screen relative space-x-10 flex flex-col md:flex-row items-center justify-center space-y-10 md:space-y-10 md:space-x-10"
+        className="py-20 min-h-screen flex flex-col md:flex-row items-center justify-center space-y-6 md:space-y-0 md:space-x-10 px-6"
       >
+        {/* Profile Picture */}
         <div className="relative p-2 rounded-full before:absolute before:inset-0 before:-m-1 before:rounded-full before:bg-gradient-to-r before:from-purple-500 before:via-blue-500 before:to-cyan-500 before:blur-lg before:opacity-75 before:animate-pulse">
           <img
             src={images}
             alt="Profile Picture"
-            className="relative rounded-full w-65 h-65 shadow-lg"
+            className="relative rounded-full w-40 h-40 md:w-65 md:h-65 shadow-lg"
           />
         </div>
+
+        {/* Text Content */}
         <div className="text-center z-10 px-4">
-          <h2 className="text-5xl bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent leading-right font-bold">
+          <h2 className="text-3xl sm:text-4xl md:text-5xl bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent font-bold">
             Hello, I'm Nyan Min Htet
           </h2>
-          <p className="mt-7 text-xl text-gray-300 max-w-lg mx-auto mb-7">
+          <p className="mt-4 text-lg sm:text-xl text-gray-300 max-w-xs sm:max-w-lg mx-auto mb-5">
             Web Developer | Network Engineer
           </p>
-          <div className="flex justify-center space-x-4">
+
+          {/* Buttons */}
+          <div className="flex flex-col sm:flex-row justify-center items-center space-y-3 sm:space-y-0 sm:space-x-4">
             <a
               href="#project"
-              className="bg-blue-600 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-1 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+              className="bg-blue-600 text-white py-3 px-6 rounded font-medium transition transform hover:-translate-y-1 hover:shadow-lg"
             >
-              View My Porject
+              View My Project
             </a>
             <a
               href="#contact"
-              className="border border-blue-600/60  text-blue-600 py-3 px-6 rounded font-medium transition-all duration-200
-              hover:-translate-y-1 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-600/30"
+              className="border border-blue-600 text-blue-600 py-3 px-6 rounded font-medium transition hover:bg-blue-600 hover:text-white"
             >
               Contact Me
             </a>
